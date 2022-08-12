93d82d214c7bfcedfcd23a541098affba2d8daa1
davibern <david.bern.pal@gmail.com>
2022-08-12 19:39:57 +0200

    Inicializar proyecto

---
+++ Comentarios/comentarios.py
@@ -0,0 +1,10 @@
+# Esto es una línea comentada
+# Esto es una segunda línea comentada
+# Esto es una tercera línea comentada
+
+"""
+Esto es una multilínea de texto
+comentad en varios saltos de línea.
+
+Como puedes ver no es necesario añadir por línea la almohadilla (#)
+"""
