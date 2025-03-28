4a029c33aabcaefac80e36b7c3f00bcb45aa40d1
davibern <david.bern.pal@gmail.com>
2025-03-28 21:31:10 +0100

    Corregir formato

--- 98_Mini_Proyectos/piedra_papel_tijera/piedra_papel_tijera.py
+++ 98_Mini_Proyectos/piedra_papel_tijera/piedra_papel_tijera.py
@@ -7,25 +7,28 @@ Se trata de jugar contra la máquina a piedra, papel, tijera.
 El usuario tendrá que elegir piedra, papel o tijera, y la máquina aleatoriamente elegirá también
 una de las tres opciones.
 '''
+
+
 def jugar_piedra_papel_tijera():
-    
+
     usuario = input('Escoje primero tu opción (piedra, papel o tijera):\n').lower()
     ia = random.choice(['piedra', 'papel', 'tijera'])
-    
+
     if (usuario == ia):
         return '⭐⭐ ¡Ha habido un empate! ⭐⭐'
-    
+
     if es_empate(usuario, ia):
         return '⭐⭐ ¡Has ganado! ⭐⭐\nLa IA ha seleccionado ' + ia
-    
+
     return '😭😭 ¡Has perdido! 😭😭\nLa IA ha seleccionado ' + ia
 
 
 '''
 Algoritmo que calcula el ganador del juego.
 '''
+
+
 def es_empate(usuario, ia):
-    
     if ((usuario == 'piedra' and ia == 'tijera')
         or (usuario == 'tijera' and ia == 'papel')
         or (usuario == 'papel' and ia == 'piedra')):
